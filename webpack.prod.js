46f229375c186549ebd608ffc90bc1eae447dd5f
Johnkoderr <alexkool3008@gmail.com>
2025-09-14 19:43:47 +0800

    Initial commit

---
+++ webpack.prod.js
@@ -0,0 +1,18 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+});
